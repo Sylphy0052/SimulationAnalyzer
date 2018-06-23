00e7701f702e5ba12b4223b35127724508e363e8
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-06-23 13:01:34 +0900

    update: draw rtt graph

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 ## sample main program
 from src.analyzer import Analyzer
 
+# Analyzerクラスを呼び出すだけの簡単なお仕事
 def main():
     analyzer = Analyzer()
+    analyzer.draw_rtt_graph()
 
 if __name__ == '__main__':
     main()
