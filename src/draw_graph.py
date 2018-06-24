e90d7868059245b6a7e8a4b06a884f7e65209224
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-06-24 15:54:39 +0900

    feat: function of draw many line graph

--- src/draw_graph.py
+++ src/draw_graph.py
@@ -49,31 +49,43 @@ def draw_rtt(data):
     plt.savefig(fig_name, dpi=90, bbox_inches="tight", pad_inches=0.0)
     plt.close('all')
 
-def draw_two_line_graph(X, Y1, Y2, labels, location, fig_name):
-    fig, ax1 = plt.subplots()
-    ln1 = ax1.plot(X, Y1, color=COLOR_LIST[0], label=ax1_label)
-    ax2 = ax1.twinx()
-    ln2 = ax2.plot(X, Y2, color=COLOR_LIST[1], label=ax2_label, linestyle=STYLE_LIST[0])
+def draw_two_line_graph(X, Y1, Y2, labels, ax_labels, location, fig_name):
+    plt.plot(X, Y1, color=COLOR_LIST[0], label=labels[0], linestype=STYLE_LIST[0])
+    plt.plot(X, Y2, color=COLOR_LIST[1], label=labels[1], linestyle=STYLE_LIST[1])
 
-    ax1.set_xlabel(X_label)
-    ax1.set_ylabel(Y1_label)
-    ax2.set_ylabel(Y2_label)
-
-    h1, l1 = ax1.get_legend_handles_labels()
-    h2, l2 = ax2.get_legend_handles_labels()
-    ax1.legend(h1+h2, l1+l2, loc=location)
+    plt.xlabel(ax_labels[0])
+    plt.ylabel(ax_labels[1])
+    plt.legend(loc=location)
 
     plt.grid(True)
 
     if max(X) > 10 ** 6:
         plt.gca().xaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
         plt.gca().ticklabel_format(style="sci",  axis="x",scilimits=(0,0))
-    if max(Y1) > 10 ** 6:
-        ax1.yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
-        ax1.ticklabel_format(style="sci",  axis="y",scilimits=(0,0))
-    if max(Y2) > 10 ** 6:
-        ax2.yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
-        ax2.ticklabel_format(style="sci",  axis="y",scilimits=(0,0))
+    if max(Y1) > 10 ** 6 or max(Y2) > 10 ** 6:
+        plt.gca().yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
+        plt.gca().ticklabel_format(style="sci",  axis="y",scilimits=(0,0))
+
+    plt.savefig(fig_name, dpi=90, bbox_inches="tight", pad_inches=0.0)
+    plt.close('all')
+
+def draw_many_line_graph(X, Y, labels, ax_labels, location, fig_name):
+    for i in range(len(Y)):
+        plt.plot(X, Y[i], color=COLOR_LIST[i], label=labels[i], linestyle=STYLE_LIST[i])
+
+    plt.xlabel(ax_labels[0])
+    plt.ylabel(ax_labels[1])
+    plt.legend(loc=location)
+
+    plt.grid(True)
+
+    if max(X) > 10 ** 6:
+        plt.gca().xaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
+        plt.gca().ticklabel_format(style="sci",  axis="x",scilimits=(0,0))
+
+    if max([max(i) for i in Y]) > 10 ** 6:
+        plt.gca().yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
+        plt.gca().ticklabel_format(style="sci",  axis="y",scilimits=(0,0))
 
     plt.savefig(fig_name, dpi=90, bbox_inches="tight", pad_inches=0.0)
     plt.close('all')
