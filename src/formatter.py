c9251de1ac9c0d40a83ab9b395777911fc48b514
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-06-24 15:55:22 +0900

    feat: function of creating plot data for draw adjust graph

--- src/formatter.py
+++ src/formatter.py
@@ -2,17 +2,21 @@
 # adjust/distance/duplication
 def dict_to_adjust_plot_data(data_dict):
     X = []
-    Y = [[] for i in range(len(data_dict.keys()))]
+    Y = []
     labels = []
 
+    from src.classify import classify_by_duplication_for_plot_data
     for adjust, datas in data_dict.items():
-        print("adjust {}".format(adjust))
-        if adjust == 0:
+        classify_dict = classify_by_duplication_for_plot_data(datas)
+        for duplication, datas in classify_dict.items():
+            label = "adjust {} duplication {}".format(adjust, duplication)
+            labels.append(label)
+            y = []
             for data in datas:
-                print(data.dat_data.distance)
-
-        elif adjust == 1:
-            for data in datas:
-                print(data.dat_data.distance)
+                x = data.dat_data.distance
+                if not x in X:
+                    X.append(x)
+                y.append(data.rtt_data.mean)
+            Y.append(y)
 
     return X, Y, labels
