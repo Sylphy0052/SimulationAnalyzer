42d35ecbc624d7f6e931a15638842c09c74e8d73
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-06-24 15:54:03 +0900

    feat: function of return dict for create plot data

--- src/classify.py
+++ src/classify.py
@@ -1,14 +1,21 @@
 from src.formatter import dict_to_adjust_plot_data
 
 def classify_by_adjust(data_dict):
-    return_arr = {}
+    return_dict = {}
     for data in data_dict.values():
         adjust = data.dat_data.adjust_num
-        if not adjust in return_arr.keys():
-            return_arr[adjust] = []
-        return_arr[adjust].append(data)
+        if not adjust in return_dict.keys():
+            return_dict[adjust] = []
+        return_dict[adjust].append(data)
 
-    return dict_to_adjust_plot_data(return_arr)
+    return dict_to_adjust_plot_data(return_dict)
 
-def classify_by_distance():
-    pass
+def classify_by_duplication_for_plot_data(datas):
+    return_dict = {}
+    for data in datas:
+        duplication = data.dat_data.duplication
+        if not duplication in return_dict.keys():
+            return_dict[duplication] = []
+        return_dict[duplication].append(data)
+
+    return return_dict
