442615cabac45a5692b93054029f9d729546325e
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-06-24 14:16:24 +0900

    fix: use function in main

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from src.analyzer import Analyzer
 # Analyzerクラスを呼び出すだけの簡単なお仕事
 def main():
     analyzer = Analyzer()
-    analyzer.draw_rtt_graph()
+    # analyzer.draw_rtt_graph()
+    analyzer.write_info_csv()
+    analyzer.draw_adjust_graph()
 
 if __name__ == '__main__':
     main()
